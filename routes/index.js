69bfbf9f8ba5060d61c55448dbc582e315ec2a10
ronald-luo <r.luo.180@gmail.com>
2022-01-14 10:59:56 -0500

    🚀 fix: error page handled /w reg exps

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/liked', function(req, res, next) {
-  res.render('liked', { title: 'spacetagram' });
+  res.render('liked');
 });
 
 router.get('/:date', function(req, res, next) {
-  res.render('page', { date: req.params.date });
-});
 
+  // Render page if date format in YYYY-MM-DD, else render error
+  if (/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/.test(req.params.date)) {
+    res.render('page', { date: req.params.date });
+  } else {
+    res.render('error');
+  }
+
+});
 
 module.exports = router;
